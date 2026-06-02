a3e8cc57c00f0545e86a6685039121abfc548067
Jens Friberg <association@live.se>
2026-06-02 18:52:42 +0200

    Create dl.py

---
+++ dl.py
@@ -0,0 +1,40 @@
+
+
+##Note from workshop:
+
+# this line is wrong. A bug added to the code
+###print(test_combined.size()) # should be torch.Size([32, 400])
+
+####### Added this section to get the files, instead of having to upload them to colab every time
+
+
+
+from urllib.request import urlopen
+from io import BytesIO
+from zipfile import ZipFile
+import os
+
+
+def download_and_unzip(url, extract_to='.'):
+    http_response = urlopen(url)
+    zipfile = ZipFile(BytesIO(http_response.read()))
+    zipfile.extractall(path=extract_to)
+
+download_and_unzip("https://raw.githubusercontent.com/Association32/NLI/main/t5-assignment-nli.zip")
+
+
+try:
+  download_and_unzip("https://raw.githubusercontent.com/Association32/NLI/main/t5-assignment-nli.zip")
+  print("data files downloaded successfully.")
+except Exception as e:
+  print(e)
+
+try:
+
+  os.remove("nli.ipynb")
+  print("nli.ipynb deleted successfully.")
+
+except Exception as e:
+  print(e)
+
+
