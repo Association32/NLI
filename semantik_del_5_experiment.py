e1b05cf7cc704df168c3193671bb647e19a95295
Jens Friberg <association@live.se>
2026-06-02 13:33:55 +0200

    Update semantik_del_5_experiment.py

--- semantik_del_5_experiment.py
+++ semantik_del_5_experiment.py
@@ -125,8 +125,8 @@ valid_data = snli["validation"].filter(valid_example)
 
 
 ### to speed thinbgs up
-train_data = train_data.select(range(50000))
-valid_data = valid_data.select(range(5000))
+train_data = train_data.select(range(10000))
+valid_data = valid_data.select(range(2000))
 
 # TOKENIZER
 
